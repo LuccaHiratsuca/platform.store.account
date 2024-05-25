bc460bd60bb0e9fed2c26c2fd6fce974c87baa63
Lucca Hiratsuca <luccahiratsuca@gmail.com>
2024-05-25 00:52:24 -0300

    feat: new methods

--- src/main/java/insper/store/account/AccountController.java
+++ src/main/java/insper/store/account/AccountController.java
@@ -6,11 +6,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "store-account")
 public interface AccountController {
+
+    @GetMapping("/accounts")
+    public ResponseEntity<AccountOut> list(
+        @RequestHeader(required = true, name = "id-user") String idUser,
+        @RequestHeader(required = true, name = "role-user") String roleUser
+    );
+
+    @GetMapping("/accounts/{id}")
+    public ResponseEntity<AccountOut> read(
+        @PathVariable(required = true) String id
+    );
 
     @PostMapping("/accounts")
     public ResponseEntity<AccountOut> create(
@@ -28,10 +40,8 @@ public interface AccountController {
         @RequestBody(required = true) AccountIn in
     );
 
-    @GetMapping("/accounts")
-    public ResponseEntity<AccountOut> read(
-        @RequestHeader(required = true, name = "id-user") String idUser,
-        @RequestHeader(required = true, name = "role-user") String roleUser
+    @DeleteMapping("/accounts/{id}")
+    public ResponseEntity<AccountOut> delete(
+        @PathVariable(required = true) String id
     );
-    
 }
